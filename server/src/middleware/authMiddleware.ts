c0cd8b1d5495ea1842be6d4a0898d1bba328442f
Pramod Boda <PB493939@Haleon.ad>
2025-02-10 21:35:57 +0530

    Implementing PERN(TS) Authentication

--- server/src/middleware/authMiddleware.ts
+++ server/src/middleware/authMiddleware.ts
@@ -1,6 +1,11 @@
 import { Request, Response, NextFunction } from "express";
 import { verifyToken } from "../utils/jwtUtils";
 
+// Define the structure of the decoded JWT payload
+interface DecodedToken {
+  userId: string; // Adjust this based on your JWT payload structure
+}
+
 export const authenticate = (
   req: Request,
   res: Response,
@@ -11,8 +16,8 @@ export const authenticate = (
     return res.status(401).json({ error: "Access denied" });
   }
   try {
-    const decoded = verifyToken(token);
-    (req as any).userId = (decoded as any).userId;
+    const decoded = verifyToken(token) as DecodedToken; // Assert the type here
+    req.userId = decoded.userId;
     next();
   } catch (error) {
     res.status(401).json({ error: error, msg: "Invalid token" });
--- server/src/types/express.d.ts
+++ server/src/types/express.d.ts
@@ -1,10 +1,10 @@
 // src/types/express.d.ts
-// import * as express from "express";
+import * as express from "express";
 
-// declare global {
-//   namespace Express {
-//     interface Request {
-//       userId?: string; // or the appropriate type (e.g., `number` or `string` depending on your implementation)
-//     }
-//   }
-// }
+declare global {
+  namespace Express {
+    interface Request {
+      userId?: string; // or the appropriate type (e.g., `number` or `string` depending on your implementation)
+    }
+  }
+}
